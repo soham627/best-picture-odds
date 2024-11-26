a9cc7c984f91f3179294668a8087d5d4b1171496
soham627 <soham.j.mall@gmail.com>
2024-11-26 14:09:14 -0500

    Add automatic scheduler to weekly_update script

--- odds-app/weekly_update.py
+++ odds-app/weekly_update.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 load_dotenv()
 engine = create_engine(os.getenv('DATABASE_URL').replace('postgresql://', 'postgresql+psycopg://'))
@@ -100,107 +101,6 @@ def find_movies(url, movies_df):
                 })
     return movies_and_predictions
 
-# URLs for each category
-base_urls = {
-    'Experts': "https://www.goldderby.com/odds/expert-odds/oscars-nominations-2025-predictions/",
-    'Star24': "https://www.goldderby.com/odds/top24-odds/oscars-nominations-2025-predictions/",
-    'Users': "https://www.goldderby.com/odds/user-odds/oscars-nominations-2025-predictions/"
-}
-
-movies_df = pd.read_csv('movies_df.csv')
-
-# scrape data
-all_data = []
-data_experts = find_movies(base_urls['Experts'], movies_df) or []
-data_star24 = find_movies(base_urls['Star24'], movies_df) or []
-data_users = find_movies(base_urls['Users'], movies_df) or []
-
-# process combined data
-combined_data = {}
-for movie in data_experts:
-    combined_data[movie['Movie Name']] = {
-        'Movie Name': movie['Movie Name'],
-        'Experts Vote': movie['Win Vote'],
-        'Experts Odds': movie['Odds'],
-        'Star24 Vote': 'N/A',
-        'Star24 Odds': 'N/A',
-        'Users Vote': 'N/A',
-        'Users Odds': 'N/A',
-        'Date': datetime.now().strftime('%Y-%m-%d')
-    }
-
-for movie in data_star24:
-    if movie['Movie Name'] in combined_data:
-        combined_data[movie['Movie Name']]['Star24 Vote'] = int(movie['Win Vote'])
-        combined_data[movie['Movie Name']]['Star24 Odds'] = movie['Odds']
-    else:
-        combined_data[movie['Movie Name']] = {
-            'Movie Name': movie['Movie Name'],
-            'Experts Vote': 'N/A',
-            'Experts Odds': 'N/A',
-            'Star24 Vote': movie['Win Vote'],
-            'Star24 Odds': movie['Odds'],
-            'Users Vote': 'N/A',
-            'Users Odds': 'N/A',
-            'Date': datetime.now().strftime('%Y-%m-%d')
-        }
-
-for movie in data_users:
-    if movie['Movie Name'] in combined_data:
-        combined_data[movie['Movie Name']]['Users Vote'] = int(movie['Win Vote'])
-        combined_data[movie['Movie Name']]['Users Odds'] = movie['Odds']
-    else:
-        combined_data[movie['Movie Name']] = {
-            'Movie Name': movie['Movie Name'],
-            'Experts Vote': 'N/A',
-            'Experts Odds': 'N/A',
-            'Star24 Vote': 'N/A',
-            'Star24 Odds': 'N/A',
-            'Users Vote': movie['Win Vote'],
-            'Users Odds': movie['Odds'],
-            'Date': datetime.now().strftime('%Y-%m-%d')
-        }
-
-all_data.extend(combined_data.values())
-
-# create df and calculate percentages and implied probabilities
-weekly_df = pd.DataFrame(all_data)
-weekly_df['pct_vote_expert'] = calculate_pct_votes(weekly_df, 'Experts Vote')
-weekly_df['pct_vote_star24'] = calculate_pct_votes(weekly_df, 'Star24 Vote')
-weekly_df['pct_vote_user'] = calculate_pct_votes(weekly_df, 'Users Vote')
-
-weekly_df['imp_prob_expert'] = weekly_df['Experts Odds'].apply(lambda x: odds_to_prob(x) if x != 'N/A' else None)
-weekly_df['imp_prob_star24'] = weekly_df['Star24 Odds'].apply(lambda x: odds_to_prob(x) if x != 'N/A' else None)
-weekly_df['imp_prob_user'] = weekly_df['Users Odds'].apply(lambda x: odds_to_prob(x) if x != 'N/A' else None)
-
-movie_names = set(movies_df['Movie Name'].tolist())
-
-## getting the betting site odds data
-api_response = requests.post(
-    "https://api.zyte.com/v1/extract",
-    auth=(os.getenv("ZYTE_API_KEY"), ""),
-    json={
-        "url": "https://www.oddschecker.com/awards/oscars/best-picture",
-        "browserHtml": True,
-    },
-)
-response_json = api_response.json()
-if "browserHtml" in response_json:
-    browser_html = response_json["browserHtml"]
-else:
-    print("Key 'browserHtml' not found in the response. Full response:")
-    print(response_json)
-
-
-soup = BeautifulSoup(browser_html, 'html.parser')
-
-# mapping the titles for movies whose names differ across the betting website and Goldderby
-title_mapping = {
-    'Joker: Folie à Deux': 'Joker: Folie a Deux',
-    'Gladiator II': 'Gladiator 2',
-    'Nickel Boys': 'The Nickel Boys',
-    'Saturday Night': 'SNL: 1975'
-}
 
 
 def find_odds_for_movie(movie_name, soup):
@@ -232,12 +132,121 @@ def find_odds_for_movie(movie_name, soup):
 
     return odds_value
 
-# assign extracted data to a column in the weekly_df records
-weekly_df['betting_odds'] = weekly_df['Movie Name'].apply(lambda name: find_odds_for_movie(name, soup))
-weekly_df['betting_pct'] = weekly_df['betting_odds'].apply(lambda x: int(odds_to_prob(x)) if x != 'N/A' and odds_to_prob(x) is not None else None)
+def weekly_movie_data_updater():
+    # URLs for each category
+    base_urls = {
+        'Experts': "https://www.goldderby.com/odds/expert-odds/oscars-nominations-2025-predictions/",
+        'Star24': "https://www.goldderby.com/odds/top24-odds/oscars-nominations-2025-predictions/",
+        'Users': "https://www.goldderby.com/odds/user-odds/oscars-nominations-2025-predictions/"
+    }
+
+    movies_df = pd.read_csv('movies_df.csv')
+
+    # scrape data
+    all_data = []
+    data_experts = find_movies(base_urls['Experts'], movies_df) or []
+    data_star24 = find_movies(base_urls['Star24'], movies_df) or []
+    data_users = find_movies(base_urls['Users'], movies_df) or []
+
+    # process combined data
+    combined_data = {}
+    for movie in data_experts:
+        combined_data[movie['Movie Name']] = {
+            'Movie Name': movie['Movie Name'],
+            'Experts Vote': movie['Win Vote'],
+            'Experts Odds': movie['Odds'],
+            'Star24 Vote': 'N/A',
+            'Star24 Odds': 'N/A',
+            'Users Vote': 'N/A',
+            'Users Odds': 'N/A',
+            'Date': datetime.now().strftime('%Y-%m-%d')
+        }
+
+    for movie in data_star24:
+        if movie['Movie Name'] in combined_data:
+            combined_data[movie['Movie Name']]['Star24 Vote'] = int(movie['Win Vote'])
+            combined_data[movie['Movie Name']]['Star24 Odds'] = movie['Odds']
+        else:
+            combined_data[movie['Movie Name']] = {
+                'Movie Name': movie['Movie Name'],
+                'Experts Vote': 'N/A',
+                'Experts Odds': 'N/A',
+                'Star24 Vote': movie['Win Vote'],
+                'Star24 Odds': movie['Odds'],
+                'Users Vote': 'N/A',
+                'Users Odds': 'N/A',
+                'Date': datetime.now().strftime('%Y-%m-%d')
+            }
+
+    for movie in data_users:
+        if movie['Movie Name'] in combined_data:
+            combined_data[movie['Movie Name']]['Users Vote'] = int(movie['Win Vote'])
+            combined_data[movie['Movie Name']]['Users Odds'] = movie['Odds']
+        else:
+            combined_data[movie['Movie Name']] = {
+                'Movie Name': movie['Movie Name'],
+                'Experts Vote': 'N/A',
+                'Experts Odds': 'N/A',
+                'Star24 Vote': 'N/A',
+                'Star24 Odds': 'N/A',
+                'Users Vote': movie['Win Vote'],
+                'Users Odds': movie['Odds'],
+                'Date': datetime.now().strftime('%Y-%m-%d')
+            }
+
+    all_data.extend(combined_data.values())
+
+    # create df and calculate percentages and implied probabilities
+    weekly_df = pd.DataFrame(all_data)
+    weekly_df['pct_vote_expert'] = calculate_pct_votes(weekly_df, 'Experts Vote')
+    weekly_df['pct_vote_star24'] = calculate_pct_votes(weekly_df, 'Star24 Vote')
+    weekly_df['pct_vote_user'] = calculate_pct_votes(weekly_df, 'Users Vote')
+
+    weekly_df['imp_prob_expert'] = weekly_df['Experts Odds'].apply(lambda x: odds_to_prob(x) if x != 'N/A' else None)
+    weekly_df['imp_prob_star24'] = weekly_df['Star24 Odds'].apply(lambda x: odds_to_prob(x) if x != 'N/A' else None)
+    weekly_df['imp_prob_user'] = weekly_df['Users Odds'].apply(lambda x: odds_to_prob(x) if x != 'N/A' else None)
+
+    movie_names = set(movies_df['Movie Name'].tolist())
+
+    ## getting the betting site odds data
+    api_response = requests.post(
+        "https://api.zyte.com/v1/extract",
+        auth=(os.getenv("ZYTE_API_KEY"), ""),
+        json={
+            "url": "https://www.oddschecker.com/awards/oscars/best-picture",
+            "browserHtml": True,
+        },
+    )
+    response_json = api_response.json()
+    if "browserHtml" in response_json:
+        browser_html = response_json["browserHtml"]
+    else:
+        print("Key 'browserHtml' not found in the response. Full response:")
+        print(response_json)
+
+    soup = BeautifulSoup(browser_html, 'html.parser')
+
+    # mapping the titles for movies whose names differ across the betting website and Goldderby
+    title_mapping = {
+        'Joker: Folie à Deux': 'Joker: Folie a Deux',
+        'Gladiator II': 'Gladiator 2',
+        'Nickel Boys': 'The Nickel Boys',
+        'Saturday Night': 'SNL: 1975'
+    }
+
+    # assign extracted data to a column in the weekly_df records
+    weekly_df['betting_odds'] = weekly_df['Movie Name'].apply(lambda name: find_odds_for_movie(name, soup))
+    weekly_df['betting_pct'] = weekly_df['betting_odds'].apply(lambda x: int(odds_to_prob(x)) if x != 'N/A' and odds_to_prob(x) is not None else None)
 
 
-# save to db
-weekly_df.to_sql('goldderby', engine, if_exists='append', index=False)
+    # save to db
+    weekly_df.to_sql('goldderby', engine, if_exists='append', index=False)
 
 
+scheduler = BlockingScheduler()
+
+# task to run every Monday at 10:00 AM
+scheduler.add_job(weekly_movie_data_updater, 'cron', day_of_week='mon', hour=10, minute=0)
+
+print("Scheduler started. Waiting for the next task...")
+scheduler.start()
