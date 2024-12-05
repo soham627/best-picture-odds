7f0d5ecd9202e4068f2b50e04f3049cbc9ee5cff
soham627 <soham.j.mall@gmail.com>
2024-12-05 10:28:02 -0600

    Add app context to create_db.py

--- odds-app/create_db.py
+++ odds-app/create_db.py
@@ -1,18 +1,20 @@
 import os
 import pandas as pd
-from app import db
+from app import db, app
+from sqlalchemy import text
 
 def initialize_database():
-    db.session.execute('DROP TABLE IF EXISTS movie_stats')
-    db.session.execute('DROP TABLE IF EXISTS goldderby')
-    db.session.commit()
+    with app.app_context():
+        db.session.execute(text('DROP TABLE IF EXISTS movie_stats'))
+        db.session.execute(text('DROP TABLE IF EXISTS goldderby'))
+        db.session.commit()
 
-    movie_stats = pd.read_csv('Database/movie_stats.csv')
-    goldderby = pd.read_csv('Database/goldderby.csv')
+        movie_stats = pd.read_csv('Database/movie_stats.csv')
+        goldderby = pd.read_csv('Database/goldderby.csv')
 
-    # Load into SQLite
-    movie_stats.to_sql('movie_stats', db.engine, index=False, if_exists='replace')
-    goldderby.to_sql('goldderby', db.engine, index=False, if_exists='replace')
+
+        movie_stats.to_sql('movie_stats', db.engine, index=False, if_exists='replace')
+        goldderby.to_sql('goldderby', db.engine, index=False, if_exists='replace')
 
 
 if __name__ == "__main__":
